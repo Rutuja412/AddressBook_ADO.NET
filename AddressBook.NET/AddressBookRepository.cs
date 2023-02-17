363e4f8c286ff583ce938a24100e83b8e88eac39
Rutuja412 <rutujakadam4545@gmail.com>
2023-02-17 21:18:43 +0530

    [Rutuja]:Create Database in Adressbook

---
+++ AddressBook.NET/AddressBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBook.NET
+{
+    internal class AddressBookRepository
+    {
+        public static string connection = @"Server=(localdb)\ProjectModels;Database=Book;Trusted_Connection=True;";
+        SqlConnection sqlConnection = new SqlConnection(connection);
+    }
+}
---
+++ AddressBook.NET/Contacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook.NET
+{
+    internal class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PhoneNumber { get; set; }
+        public int zipcode { get; set; }
+        public string EMaiilID { get; set; }
+        public string Book_Name { get; set; }
+        public string Contact_Type { get; set; }
+    }
+}
